398a7a9d351b87b1e92a4719c4d34c0b76512aff
rajat <rkumar201@sapient.com>
2021-10-12 06:04:46 -0500

    initial commit

---
+++ Assets/Scripts/PoolBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolBall : MonoBehaviour
+{
+    public AudioSource ballHitSound;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter (Collision collision) {
+        Debug.Log($"{gameObject.name} is colliding with {collision.collider.name}");
+        if (!ballHitSound.isPlaying && collision.collider.name == "PoolBall") {
+            ballHitSound.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/pagePrint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pagePrint : MonoBehaviour
+{
+    public GameObject page;
+    public Transform spawnTransform;
+    public AudioSource sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("CreatePage", 3f, 20f);
+    }
+
+    void CreatePage() {
+        Debug.Log("spawned a page");
+        Instantiate(page, spawnTransform.position, spawnTransform.rotation);
+        if(!sound.isPlaying) {
+            sound.Play();
+        }
+    }
+}
