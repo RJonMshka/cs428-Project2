63c9cf814ffa2e8583cabc310b754680705f8874
rajat <rkumar201@sapient.com>
2021-10-16 11:59:11 -0500

    added few items to outer env

---
+++ Assets/Scripts/ManagerBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerBehavior : MonoBehaviour
+{
+
+    private bool isAngry = false;
+    static Animator anim;
+    private AudioSource angryAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        angryAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if(!angryAudio.isPlaying && !isAngry && other.name == "ExampleAvatar") {
+            isAngry = true;
+            anim.SetTrigger("playAngry");
+            Invoke("SetAngryOff", 3f);
+            angryAudio.Play();
+        }
+    }
+
+    void SetAngryOff() {
+        isAngry = false;
+    }
+}
